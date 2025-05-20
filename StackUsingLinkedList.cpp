6c5b25cce8dc11e94d6bd817a10d1b75e9f09dbc
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-20 15:16:03 +0700

    membuat element pada void peek

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -41,4 +41,23 @@ int push(int value)
         cout << "Popped Value: " << top->data << endl;
         top = top->next;
     }
+    
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } 
+    }
+    
 };
