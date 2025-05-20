72467ff5d936341d828eae496cfdfc6d6bcdfb15
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-20 10:14:05 +0700

    add library

---
+++ stacUsingLinkedlist.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+   int data;
+   Node *next;
+
+   Node()
+   {
+    next = NULL;
+   }
+};
+
+class Stak
+{
+private:
+     Node *top;
+
+public:
+    Stak()
+    {
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty" << endl;
+        }
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+    }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+    }
+};
+
