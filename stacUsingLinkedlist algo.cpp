e0c69575c4f766a3da9764efbd66ab69c381c770
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-20 13:48:57 +0700

    fungsi utama

--- stacUsingLinkedlist algo.cpp
+++ stacUsingLinkedlist algo.cpp
@@ -20,5 +20,57 @@ public:
     {
         top = NULL;
     }
+ int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
 
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty" << endl;
+        }
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+    }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } 
+    }
+
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+};
+
+int main()
+
+
+
+
+
+
+
+  
    
