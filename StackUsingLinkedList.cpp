6189aa54f87cb79731571fb80f1b753d3bc5e0f0
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-20 15:10:04 +0700

    add new node

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -24,6 +24,11 @@ public:
 int push(int value)
     {
         Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
         
     }
 };
